cbff51b77b9ec576765f314ba211dde7acca8a87
Ayneesaranya <ayneesaranya22@gmail.com>
2025-12-19 15:31:17 +0700

    สร้างไฟล์index.php

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="th">
+<head>
+    <meta charset="UTF-8">
+    <title>My PHP</title>
+</head>
+<body>
+    <h2>สวัสดีค่ะ</h2>
+
+    <?php
+        $name = "Aineesaranya";
+        echo "ชื่อของฉันคือ $name";
+    ?>
+</body>
+</html>
